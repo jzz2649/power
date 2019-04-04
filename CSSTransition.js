0008ed37f73be0e737decfa751c27abee1b9fd21
jzz2649 <1563382649@qq.com>
2019-04-04 14:37:42 +0800

    add CSSTransition

---
+++ CSSTransition.js
@@ -0,0 +1,59 @@
+class CSSTransition {
+    static nextRAF(callback){
+        requestAnimationFrame(()=>{
+			requestAnimationFrame(callback)
+        })
+    }
+    constructor(options={}){
+        const must = ['el', 'className'];
+        for(const key of must){
+            if(!options.hasOwnProperty(key)){
+                throw new Error(`${key} can not be empty.`)
+            }
+        }
+        this.options = {
+            onEntering(){},
+            onEntered(){},
+            onExiting(){},
+            onExited(){}
+        };
+        Object.assign(this.options, options);
+        this.enter();
+    }
+    addClassName(status){
+        const className = this.options.className + '-' + status;
+        this.options.el.classList.add(className);
+    }
+    onCallback(callback){
+        const allStyle = window.getComputedStyle(this.options.el);
+        let duration = parseFloat(allStyle['transitionDuration']);
+        if(Number.isNaN(duration)){
+            duration = 0;
+        }
+        duration *= 1000;
+        setTimeout(callback, duration);
+    }
+    enter(){
+        this.addClassName('enter');
+        this.options.onEntering();
+        CSSTransition.nextRAF(()=>{
+            this.addClassName('enter-active');
+            this.onCallback(()=>{
+                this.options.onEntered();
+            })
+        })
+    }
+    exit(){
+        this.addClassName('exit');
+        this.options.onExiting();
+        CSSTransition.nextRAF(()=>{
+            this.addClassName('exit-active');
+            this.onCallback(()=>{
+                this.options.onExited();
+            });
+        })
+    }
+    remove(){
+        this.exit();
+    }
+}
