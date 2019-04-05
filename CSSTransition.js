21545fb4facc19a980301f8664b9d4372f39bb7d
jzz2649 <1563382649@qq.com>
2019-04-06 01:47:30 +0800

    修复提前结束bug

--- CSSTransition.js
+++ CSSTransition.js
@@ -1,7 +1,7 @@
 class CSSTransition {
-    static nextRAF(callback){
+    static nextRAF(callback, status){
         requestAnimationFrame(()=>{
-            requestAnimationFrame(callback);
+            requestAnimationFrame(()=>callback(status));
         })
     }
     constructor(options={}){
@@ -18,6 +18,8 @@ class CSSTransition {
             onExited(){}
         };
         Object.assign(this.options, options);
+        this._enterStatus = false;
+        this._exitStatus = false;
     }
     addClassName(status){
         const className = this.options.className + '-' + status;
@@ -36,28 +38,48 @@ class CSSTransition {
             duration = 0;
         }
         duration *= 1000;
-        setTimeout(callback, duration);
+        return setTimeout(callback, duration);
     }
     enter(){
+        if(this._exitStatus){
+            clearTimeout(this._exitTimer);
+            this.removeClassName('exit', 'exit-active');
+            this.options.onExited();
+            this._exitStatus = false;
+        }
+        this._enterStatus = true;
         this.addClassName('enter');
         this.options.onEntering();
-        CSSTransition.nextRAF(()=>{
-            this.addClassName('enter-active');
-            this.onCallback(()=>{
-                this.removeClassName('enter', 'enter-active');
-                this.options.onEntered();
-            })
-        })
+        CSSTransition.nextRAF((status)=>{
+            if(status){
+                this.addClassName('enter-active');
+                this._enterTimer = this.onCallback(()=>{
+                    this.removeClassName('enter', 'enter-active');
+                    this.options.onEntered();
+                    this._enterStatus = false;
+                })
+            }
+        }, this._enterStatus)
     }
     exit(){
+        if(this._enterStatus){
+            clearTimeout(this._enterTimer);
+            this.removeClassName('enter', 'enter-active');
+            this.options.onEntered();
+            this._enterStatus = false;
+        }
+        this._exitStatus = true;
         this.addClassName('exit');
         this.options.onExiting();
-        CSSTransition.nextRAF(()=>{
-            this.addClassName('exit-active');
-            this.onCallback(()=>{
-                this.removeClassName('exit', 'exit-active');
-                this.options.onExited();
-            });
-        })
+        CSSTransition.nextRAF((status)=>{
+            if(status){
+                this.addClassName('exit-active');
+                this._exitTimer = this.onCallback(()=>{
+                    this.removeClassName('exit', 'exit-active');
+                    this.options.onExited();
+                    this._exitStatus = false;
+                });
+            }
+        }, this._exitStatus)
     }
 }
