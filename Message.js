62ba48fcdfb99374ed928879e05ff9bb1cd1d252
jzz2649 <1563382649@qq.com>
2019-04-06 12:24:56 +0800

    添加例子

--- Message.js
+++ Message.js
@@ -1,80 +1,91 @@
+/**
+ * @method creEle from https://github.com/jzz2649/utils/blob/master/creEle.js
+ * @method CSSTransition from https://github.com/jzz2649/power/blob/master/CSSTransition.js
+ */
+
+const MSG_STYLE_NAME = Symbol('stylesheet');
+
+const MSG_STYLE_TEXT = `
+.msg-box {
+	position: fixed;
+	left: 0px;
+	top: 0px;
+	width: 100%;
+	pointer-events: none;
+}
+.msg-item {
+	width: 100%;
+	margin: 10px;
+	text-align: center;
+	pointer-events: none;
+	transform: translateY(0px);
+	will-change: transform;
+}
+.msg-content {
+	display: inline-block;
+	padding: 10px 16px;
+	background: white;
+	border-radius: 4px;
+	box-shadow: rgba(0, 0, 0, 0.15) 0px 4px 12px;
+	pointer-events: all;
+}
+.msg-enter {
+	transform: translateY(-20px);
+}
+.msg-enter-active {
+	transform: translateY(0);
+	transition: transform 0.2s ease-out;
+}
+.msg-exit {
+	transform: translateY(0);
+}
+.msg-exit-active {
+	transform: translateY(-20px);
+	transition: transform 0.2s ease-out;
+}
+`
+
 class Message {
-	static getEl(tagName){
-		return document.createElement(tagName);
-	}
-	static nextRAF(callback){
-		requestAnimationFrame(()=>{
-			requestAnimationFrame(()=>{
-				callback();
-			})
-		})
-	}
 	constructor(options){
 		this.options = {
 			root: document.body,
 			delay: 3000
 		};
 		Object.assign(this.options, options);
+		if(!window[MSG_STYLE_NAME]){
+			window[MSG_STYLE_NAME] = true;
+			document.head.appendChild(creEle('style', null, MSG_STYLE_TEXT));
+		}
 		this.count = 0;
-		this.trunk = Message.getEl('div');
-		Object.assign(this.trunk.style,{
-			position: 'fixed',
-			left: 0,
-			top: 0,
-			width: '100%',
-			pointerEvents: 'none'
-		})
-	}
-	entry(item){
-		Message.nextRAF(()=>{
-			Object.assign(item.style,{
-				transitionDuration: '0.2s',
-				transitionProperty: 'transform',
-				transitionTimingFunction: 'ease-out',
-				transform: 'translateY(0)'	
-			})		
-		})
-	}
-	leave(item){
-		item.style.transform = 'translateY(-20px)';
-		setTimeout(()=>{
-			this.trunk.removeChild(item);
-			this.count--;
-			if(this.count === 0){
-				this.options.root.removeChild(this.trunk);
-			}
-		}, 100)
+		this.trunk = creEle('div',{className:'msg-box'});
 	}
 	add(msg, delay){
-		this.count++;
 		delay = delay || this.options.delay;
-		const item = Message.getEl('div');
-		const content = Message.getEl('div');
-		Object.assign(item.style, {
-			width: '100%',
-			margin: '10px',
-			textAlign: 'center',
-			pointerEvents: 'none',
-			transform:'translateY(-20px)',
-			willChange: 'transform'
-		})
-		Object.assign(content.style, {
-			display: 'inline-block',
-			padding: '10px 16px',
-			background: 'white',
-			borderRadius: '4px',
-			boxShadow: '0 4px 12px rgba(0, 0, 0, 0.15)',
-			pointerEvents: 'all'
-		})
-		content.textContent = msg;
-		item.appendChild(content);
-		this.trunk.appendChild(item);
-		if(this.count===1){
-			this.options.root.appendChild(this.trunk);
+		const content = creEle('div',{className:'msg-content'}, msg);
+		const item = creEle('div',{className:'msg-item'}, content);
+		const enter = () => {
+			if(!this.count){
+				this.options.root.appendChild(this.trunk);
+			}
+			this.trunk.appendChild(item);
+			this.count++;
 		}
-		this.entry(item);
+		const exit = () => {
+			this.trunk.removeChild(item);
+			this.count--;
+			if(!this.count){
+				this.options.root.removeChild(this.trunk);
+			}
+		}
+		const csstransition = new CSSTransition({
+			el: item,
+			className: 'msg',
+			onEntering: enter,
+			onExited: exit
+		})
+		csstransition.enter();
 		setTimeout(()=>{
-			this.leave(item);
+			csstransition.exit();
 		}, delay);
 	}
 }
---
+++ example/creEle.js
@@ -0,0 +1,40 @@
+/**
+ * @param {string} type
+ * @param {object} props
+ * @param {...string|HTMLElement} childrens
+ * @return {HTMLElement}
+ */
+
+function creEle(type, props, children) {
+    var node = document.createElement(type);
+    var childs = [].slice.call(arguments, 2);
+  
+    if (toString.call(childs[0]) === "[object Array]") {
+      childs = childs[0];
+    }
+  
+    function toEle(child) {
+      if (child instanceof HTMLElement) {
+        return child;
+      }
+      return document.createTextNode(String(child));
+    }
+  
+    function merge(attr, prop) {
+      for (var key in prop) {
+        if (toString.call(prop[key]) === "[object Object]") {
+          merge(attr[key], prop[key]);
+        } else {
+          attr[key] = prop[key];
+        }
+      }
+    }
+  
+    merge(node, props);
+  
+    for (var i = 0; i < childs.length; i++) {
+      node.appendChild(toEle(childs[i]));
+    }
+  
+    return node;
+  }
